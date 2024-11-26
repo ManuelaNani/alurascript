0914f2b375bf05a7a446a387b1fc536e8881bc53
ManuelaNani <dasilvananimanuela@gmail.com>
2024-11-25 21:53:00 -0300

    Projeto concluído.

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Educação',
+    'Como o uso de inteligência artificial pode transformar o ensino tradicional nas escolas?',
+    'A inteligência artificial pode personalizar o aprendizado ao identificar as necessidades individuais de cada aluno, oferecendo conteúdos e exercícios adaptados ao seu ritmo. Além disso, ferramentas como tutores virtuais e análises preditivas ajudam os professores a identificar dificuldades, promovendo um ensino mais eficaz.'
+)
+
+criaCartao(
+    'Saúde',
+    'Quais são os impactos positivos da telemedicina na sociedade?',
+    'A capital da França é A telemedicina facilita o acesso a cuidados médicos, especialmente em áreas remotas. Ela permite consultas rápidas, monitoramento contínuo de condições crônicas e até diagnósticos baseados em inteligência artificial. Além disso, reduz custos com deslocamento e aumenta a eficiência no atendimento.'
+)
+
+criaCartao(
+    'Meio Ambiente',
+    'Qual é a relação entre o desmatamento e a crise hídrica?',
+    'O desmatamento reduz a capacidade das florestas de regular o ciclo da água, diminuindo a evapotranspiração e a formação de chuvas. Isso impacta diretamente os reservatórios de água e pode agravar crises hídricas, especialmente em regiões já vulneráveis, como o sudeste do Brasil.'
+)
+
+criaCartao(
+    'Tecnologia',
+    'Como o avanço da computação quântica pode impactar a segurança digital?',
+    'A computação quântica tem o potencial de quebrar os métodos atuais de criptografia, que são baseados em algoritmos difíceis de resolver com computadores tradicionais. No entanto, também oferece a possibilidade de criar novos sistemas de segurança praticamente invioláveis, baseados na mecânica quântica.'
+)
+
+criaCartao(
+    'Psicologia',
+    'Por que a prática da meditação é recomendada para melhorar a saúde mental?',
+    'A meditação ajuda a reduzir o estresse e a ansiedade, além de melhorar a concentração e a clareza mental. Estudos mostram que ela altera a atividade cerebral, promovendo maior equilíbrio emocional e até mesmo fortalecendo áreas associadas à empatia e à autocompaixão.  '
+)
